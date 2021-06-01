1b4a7db1b453e3b33ebc97fe37ae80c43ace83b0
aviator19941 <aviator1994@gmail.com>
2021-06-01 12:07:15 -0700

    Expressions in JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 function App() {
+  const name = 'Brad'
+  const x = true
+
   return (
     <div className="container">
       <h1>Hello from React</h1>
-      <h2>Hello</h2>
+      <h2>Hello {name}</h2>
+      <h2>Hello {1 + 1}</h2>
+      <h2>Hello {x ? 'Yes' : 'No'}</h2>
     </div>
   );
 }
