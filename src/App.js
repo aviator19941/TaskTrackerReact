a9eaa36aefbb1dc3b0389ae2c36c9e7c89b1b406
aviator19941 <aviator1994@gmail.com>
2021-06-02 15:32:10 -0700

    Toggle reminder on server

--- src/App.js
+++ src/App.js
@@ -25,6 +25,14 @@ function App() {
     return data
   }
 
+  // Fetch One Task
+  const fetchTask = async (id) => {
+    const response = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await response.json()
+
+    return data
+  }
+
   // Add Task
   const addTask = async (task) => {
     const response = await fetch('http://localhost:5000/tasks',
@@ -56,9 +64,22 @@ function App() {
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updateTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const response = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updateTask)
+    })
+
+    const data = await response.json()
+
     setTasks(
-      tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task )
+      tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task )
     )
   }
 
